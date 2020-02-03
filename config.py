eb73baff7d45a9deef996135de499c1e6e48ddcb
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2020-02-03 13:35:36 +0000

    Update config.py
    If you wish to add more questions to the quiz database, fill in and copy out the default placeholder in the questions array.

--- config.py
+++ config.py
@@ -10,15 +10,14 @@ import sqlite3
 conn = sqlite3.connect('Quiz.db')
 
 c = conn.cursor()
-
+"""
 c.execute('''CREATE TABLE comp_quiz
              (Question, Correct_Ans, Ans_1, Ans_2, Ans_3, Ans_4)''')
+"""
 
-questions = [('What are computer chips usually made of?','2','Iron','Silicon','Copper','Gold'),
-             ('How many bytes are in one Kilobyte?','1','1024','1000','1023','1000'),
-             ('What does LAN stand for?','2','Logical Access Network','Local Area Network','Logic And Numbers','Local Access Netowrk'),
-             ('What does the CPU do?','4','Stores Data','Processes Data','Controls Hardware','Performs Logic Functions'),
-             ('When does RAM dump its data?','3','Never','When there is no RAM left','When the system turns off','When it is removed')]
+# Use the placeholder below to add more questions and answers to the quiz.
+questions = [('Question','Correct Answer (number)','Answer','Answer','Answer','Answer')]
+
 c.executemany('INSERT INTO comp_quiz VALUES (?,?,?,?,?,?)', questions)
 
 conn.commit()
