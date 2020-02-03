8e1c96ce8f42b6b96aadf6e134758fe56daed6d1
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2020-02-03 17:16:47 +0000

    Update game.py

--- game.py
+++ game.py
@@ -1,12 +1,36 @@
 import sqlite3
+from random import randint
 
-questions = []
+Questions = []
 Answers = []
+Cor_Ans = []
 
-conn = sqlite3.connect('Quiz.db')
-c = conn.cursor()
 
-for row in c.execute('SELECT Question FROM comp_quiz'):
-    questions.append(row)
+def data_load():
 
-print(questions[0])
+    conn = sqlite3.connect('Quiz.db')
+    c = conn.cursor()
+
+    for row in c.execute('SELECT Question FROM comp_quiz'):
+        Questions.append(row)
+    for row in c.execute('SELECT Correct_Ans FROM comp_quiz'):
+        Cor_Ans.append(row)
+    for row in c.execute('SELECT Ans_1, Ans_2, Ans_3, Ans_4 FROM comp_quiz'):
+        Answers.append(row)
+
+
+'''
+Each list must be treated as a multidimensional array, even if they only have one value.
+Only way to present the info is as below
+
+print(Questions[0][0])
+print(Cor_Ans[0][0])
+print(Answers[0][0])
+'''
+
+order = randint(0, len(Questions))
+
+if __name__ == '__main__':
+    print("Multiple Choice Quiz")
+    print("Please Select a connected database:")
+    print()
